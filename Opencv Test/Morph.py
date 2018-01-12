a1644e45115ee9db4bb26d38b16174d08fe92849
JoongGoo-Lee <jglee7201@gmail.com>
2018-01-12 13:03:01 +0900

    영상을 Blur 처리하는 코드

--- Opencv Test/Morph.py
+++ Opencv Test/Morph.py
@@ -9,10 +9,10 @@ import cv2
 def Morph():
     img = cv2.imread('C:/Users/jglee/Desktop/IMAGES/KATARI2_480_270.mp4_20180105_142941.742.jpg', cv2.IMREAD_GRAYSCALE)
 
-    kernel = np.ones((3,3), np.uint8)
+    kernel = np.ones((2,2), np.uint8)
 
     opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-    closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
 
     cv2.imshow('opening', opening)
     cv2.imshow('closing', closing)
