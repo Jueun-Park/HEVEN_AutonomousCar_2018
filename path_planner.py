c91b2e3af4cb9740037db0fd867dbbed474c2b77
홍빈 <dtb05045@gmail.com>
2018-01-15 18:42:08 +0900

    Parabola class, Lane class 수정, curve plot 함수 추가

--- path_planner.py
+++ path_planner.py
@@ -1,7 +1,7 @@
 # 경로 설정
 # input: 1. numpy array (from lidar)
 #        2. numpy array (from lane_cam)
-# output: 경로가 표시된 numpy array (to car_control)
+# output: 차선 center 위치, 기울기, 곡률이 담긴 numpy array
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,14 +14,13 @@ class Parabola:
         self.a1 = a1
         self.a2 = a2
 
-    def plot(self, color):
-        x = np.arange(-200, 0)
-        y = [self.a0 + self.a1 * value + self.a2 * value ** 2 for value in x]
-        plt.plot(x, y, color)
-
     def get_curvature(self, x):
         curvature = 2 * self.a2 / pow((1 + (self.a1 + 2 * self.a2 * x) ** 2), 1.5)
         return curvature
+
+    def get_derivative(self, x):
+        derivative = self.a1 = 2 * self.a2 * x
+        return derivative
 
     def get_value(self, x):
         value = self.a0 + self.a1 * x + self.a2 * x ** 2
@@ -33,8 +32,15 @@ class Lane:
     def __init__(self, left_coeffs, right_coeffs):
         self.left_coeffs = left_coeffs
         self.right_coeffs = right_coeffs
-        self.trajectory = Parabola((left_coeffs[0] + right_coeffs[0]) / 2,
-                                   (left_coeffs[1] + right_coeffs[1]) / 2, (left_coeffs[2] + right_coeffs[2]) / 2)
+
+        self.trajectory = Parabola(
+            (left_coeffs[0] + right_coeffs[0]) / 2,
+            (left_coeffs[1] + right_coeffs[1]) / 2,
+            (left_coeffs[2] + right_coeffs[2]) / 2)
+
+    def set_coeffs(self, left_coeffs, right_coeffs):
+        self.left_coeffs = left_coeffs
+        self.right_coeffs = right_coeffs
 
     def set_trajectory(self, ratio_1, ratio_2):
         self.trajectory = Parabola(
@@ -43,3 +49,40 @@ class Lane:
             (self.left_coeffs[2] * ratio_1 + self.right_coeffs[2] * ratio_2) / (ratio_1 + ratio_2))
 
     def get_trajectory(self): return self.trajectory
+
+    def plot_lanes(self):
+        x = np.arange(-200, 0)
+        left = [self.left_coeffs[0] + self.left_coeffs[1] * value + self.left_coeffs[2] * value ** 2 for value in x]
+        right = [self.right_coeffs[0] + self.right_coeffs[1] * value + self.right_coeffs[2] * value ** 2 for value in x]
+        center = [self.trajectory.a0 + self.trajectory.a1 * value + self.trajectory.a2 * value ** 2 for value in x]
+
+        plt.plot(x, left)
+        plt.plot(x, right)
+        plt.plot(x, center)
+
+    def get_lane_status(self):
+        measure_location = -25
+        lane_center = self.get_trajectory().get_value(measure_location)
+        lane_slope = self.get_trajectory().get_derivative(measure_location)
+        lane_curvature = self.get_trajectory().get_curvature(measure_location)
+
+        lane_status = np.array([lane_center, lane_slope, lane_curvature])
+        return lane_status
+
+
+def plot_curve(lanes):  # lanes: Lane instance
+    plt.figure(figsize=(6, 6))
+    plt.ylim(-100, 100)
+    plt.xlim(-200, 0)
+
+    lanes.plot_lanes()
+
+    plt.show()
+
+left_coeffs = np.array([60, -0.05, 0.0005])
+right_coeffs = np.array([-60, -0.025, 0.00075])
+
+current_lane = Lane(left_coeffs, right_coeffs)
+plot_curve(current_lane)
+print(current_lane.get_lane_status())
+
