58a9992523ce51f4f9185db37c47d92b0b1e0a47
Junheyok, Park <vzx00770@naver.com>
2018-01-17 18:15:01 +0900

    차량 제어 default(장애물이 없는 일반 직선 및 곡선 주행)코드 작성

--- car_control.py
+++ car_control.py
@@ -5,3 +5,120 @@
 # output: 통신 패킷 만드는 데 필요한 정보 (to communication)
 
 import numpy
+import cv2
+import time
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+global destination, lanewidth_Half, lane_Error, destination_I, list_Lane
+global gear, speed_Obs, steer_past
+gear = 0
+steer = 0
+steer_past = 0
+obst_speed = 54
+main_speed = 54
+
+
+################Function#######################
+def steering(Mission, ch, Obstacle, dotted_Line, curvature, linear, cross_track_error_1, cross_track_error_2,
+             steer_measured_Comb):  # , speed_Default, speed_Obs):
+
+    global steer, gear, speed_Obs, speed_Default
+    global steer_past
+    check = Mission * ch
+
+    ################### U-TURN ##################################
+    if check == 5:
+        steer = 0
+        ch = 2
+        speed_Obs = 40
+        Mission = 9
+        return ch, steer, speed_Obs, Mission  # , gear
+
+    ################### S-CURVE #################################
+    elif check == 0:
+        speed_Obs = 30
+        front_dis = 0.30  ## 0.30m라고 가정
+        car_dis = front_dis + car_front
+        all_dis_1 = round(pow(car_dis, 2), 2) + round((cross_track_error / 100, 2), 2)
+        all_dis_2 = round(math.sqrt(all_dis_1), 2)
+        car_front = 0.28
+        velocity = 0.83
+        tan_value = linear / (front_dis + car_front)
+        theta_1 = math.degrees(math.atan(tan_value))
+        speed_Default = main_speed
+        k = 1
+        if -15 < theta_1 < 15:
+            if abs(cross_track_error) / 100 < 0.16:
+                k = 0.5
+        theta_2 = math.degrees(math.atan((k * cross_track_error) / velocity))
+        steer_now = theta_1 + theta_2
+        adjust - 0.3
+        steer_final = (adjust * steer_past) + ((1 - adjust) * steer_now)
+        steer = steer_final * 71
+        steer_past = steer_final
+        if steer > 1970:
+            steer = 1970
+        elif steer < -1970:
+            steer = -1970
+        ##        print(cross_track_error)
+        ##        print(steer_final)
+        ##        print(steer)
+        return ch, steer, speed_Obs, Mission  # , gear
+
+
+    ################  JU - CHA ##################################
+
+    elif check == 7:
+        steer = 0
+        gear = 0
+        check = 10
+        return ch, steer, speed_Obs, Mission  # , gear
+
+    ################ CROSS - WALK  ###############################
+
+    elif check == 7:
+        steer = 0
+        gear = 0
+        check = 10
+        return ch, steer, speed_Obs, Mission  # , gear
+
+    ################ Moving - Obs  ###############################
+
+    elif check == 7:
+        steer = 0
+        gear = 0
+        check = 10
+        return ch, steer, speed_Obs, Mission  # , gear
+
+    ################ default ######################################
+
+    else:
+        front_dis = 1  ## 1m라고 가정
+        car_dis = front_dis + car_front
+        all_dis_1 = round(pow(car_dis, 2), 2) + round(pow(cross_track_error / 100, 2), 2)
+        all_dis_2 = round(math.sqrt(all_dis_1), 2)
+        car_front = 0.28
+        velocity = 1.5
+        tan_value = (linear * (-1)) / (front_dis + car_front)
+        theta_1 = math.degrees(math.atan(tan_value))
+        speed_Default = main_speed
+        k = 1
+        if -15 < theta_1 < 15:
+            if abs(cross_track_error) / 100 < 0.27:
+                k = 0.5
+        theta_2 = math.degrees(math.atan((k * cross_track_error_1) / velocity))
+        steer_now = theta_1 + theta_2
+        adjust - 0.3
+        steer_final = (adjust * steer_past) + ((1 - adjust) * steer_now)
+        steer = steer_final * 71
+        steer_past = steer_final
+        if steer > 1970:
+            steer = 1970
+        elif steer < -1970:
+            steer = -1970
+        ##        print(cross_track_error)
+        ##        print(steer_final)
+        print(steer)
+        return ch, steer, speed_Default, Mission  # , gear
