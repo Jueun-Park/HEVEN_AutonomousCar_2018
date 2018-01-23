9cfbb955ff5358d0937a13c27773b746ecde4971
JoongGoo-Lee <jglee7201@gmail.com>
2018-01-23 19:10:52 +0900

    2018/01/23

--- Opencv Test/180117.py
+++ Opencv Test/180117.py
@@ -1,60 +1,26 @@
-# -*- Encoding:UTF-8 -*- #
-'''
-
-2018/01/17 에 만든 Code
-Video 에서 Bird-Eye-View 를 적용 후 Gaussian Filter & Opening Filter 적용
-이후 Canny Edge Detection을 이용해 Edge를 추출 후 Houghline 그리기.
-
-'''
-
+#-*- coding:utf-8 -*-
 import cv2
 import numpy as np
 
+img = cv2.imread('C:/Users/jglee/Desktop/IMAGES/KATARI2_480_270.mp4_20180105_142935.309.jpg')
 
-def lane_Detection():
+# 행 : Height, 열:width
+height, width = img.shape[:2]
 
-    try:
-        print('카메라를 구동합니다.')
-        cap = cv2.VideoCapture('C:/Users/jglee/Desktop/VIDEOS/project_video.mp4')
-    except:
-        print('카메라 구동 실패')
-        return
-    #나중에 Cam 을 실행하게 되면 크기를 결정. 지금은 640x480
-    ret = cap.set(3, 640)
-    ret = cap.set(4, 480)
+# 이미지 축소
+shrink = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 
-    while True:
-        ret, frame = cap.read()
+# Manual Size지정
+zoom1 = cv2.resize(img, (width*2, height*2), interpolation=cv2.INTER_CUBIC)
 
-        if not ret:
-            print('비디오 읽기 오류')
-            break
-
-        #GRAYSCALE로 바꾼 함수.
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('Output',gray)
-
-        ROI = gray[480:720, 0:1200]
-        cv2.imshow('ROI',ROI)
-
-        #Bird_View에 대한 함수.
-        h, w = ROI.shape[:2]
-
-        pts1 = np.float32([[0, 0], [1280, 0], [0, 720], [1280, 720]])
-        pts2 = np.float32([[0, 0], [1280, 0], [480, 720], [800, 720]])
-
-        M = cv2.getPerspectiveTransform(pts1, pts2)
-
-        img = cv2.warpPerspective(ROI, M, (w, h))
-
-        cv2.imshow('Bird_View', img)
+# 배수 Size지정
+zoom2 = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
 
 
-        k = cv2.waitKey(1) & 0xFF
-        if k == 27:
-            break
+cv2.imshow('Origianl', img)
+cv2.imshow('Shrink', shrink)
+cv2.imshow('Zoom1', zoom1)
+cv2.imshow('Zoom2', zoom2)
 
-    cap.release()
-    cv2.destroyAllWindows()
-
-lane_Detection()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
