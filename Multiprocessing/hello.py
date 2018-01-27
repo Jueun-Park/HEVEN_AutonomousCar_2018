6292b52d8ae5bf786667a974affbef1ef6796978
zenon4689 <zenon4689@gmail.com>
2018-01-27 23:54:31 +0900

    연습

--- Multiprocessing/hello.py
+++ Multiprocessing/hello.py
@@ -4,10 +4,10 @@ import os
 
 
 def test(i) :
-    time.sleep(2)
+    time.sleep(1)
     print("hello~~"+str(i))
     os.system("cmd.exe /c start echo hello")#cmd창 띄우기
-    time.sleep(2)
+    time.sleep(1)
 
 
 def main() :
@@ -15,11 +15,18 @@ def main() :
     procs=[]
 
     for i in range(4) :
-        p=multiprocessing.Process(target=test,args=(i,))
-        procs.append(p)
-        p.start()
+        p1=multiprocessing.Process(target=test,args=(i,))
+        p2 = multiprocessing.Process(target=test, args=(i,))
+        procs.append(p1)
+        p1.start()
+        p2.start()
 
-    for p in procs :
-        p.join()
+    for p1 in procs :
+        p1.join()
+        p2.join()
 if __name__=="__main__" :
+    start_time = time.time()
     main()
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    #순차적으로 처리되지 않는경우가 보임
