c68b02c442f65db9f01fb122ca23beb1a14dc02c
홍빈 <dtb05045@gmail.com>
2018-03-06 21:00:24 +0900

    get_data에서 print를 return으로 바꿈

--- lidar.py
+++ lidar.py
@@ -3,14 +3,50 @@
 # output: numpy array? (to path_planner)
 
 import math
-import numpy
 import socket
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib.lines import Line2D
 import threading
 import time
 
+modes = {'DEFAULT': 0, 'PARKING': 1, 'STATIC_OBS': 2,
+         'MOVING_OBS': 3,'S_CURVE': 4, 'NARROW': 5, 'U_TURN': 6, 'CROSS_WALK': 7}
+
 class Lidar:
+
+    # 미션별 ROI에 쓰이는 상수와 Plotting에 쓰이는 선분들
+    # 1) 정적 장애물
+    STATIC_WIDTH = 60
+    STATIC_HEIGHT = 50
+
+    # 2) 동적 장애물
+    MOVING_WIDTH = 70
+    MOVING_HEIGHT = 30
+
+    # 3) S_curve
+    S_WIDTH = 80
+    S_HEIGHT = 20
+
+    # 4) Narrow
+    NARROW_WIDTH = 70
+    NARROW_HEIGHT = 70
+
+    line0 = Line2D([-50, -50, 50, 50], [-100, -4, -4, -100], color = 'k')
+
+    line1 = Line2D([-STATIC_WIDTH, -STATIC_WIDTH, STATIC_WIDTH, STATIC_WIDTH, -STATIC_WIDTH],
+                   [0, STATIC_HEIGHT * 3, STATIC_HEIGHT * 3, 0, 0], color = 'b')
+    line2 = Line2D([-STATIC_WIDTH, STATIC_WIDTH], [STATIC_HEIGHT, STATIC_HEIGHT], color = 'b')
+    line3 = Line2D([-STATIC_WIDTH, STATIC_WIDTH], [STATIC_HEIGHT * 2, STATIC_HEIGHT * 2], color = 'b')
+
+    line4 = Line2D([-MOVING_WIDTH, -MOVING_WIDTH, MOVING_WIDTH, MOVING_WIDTH, -MOVING_WIDTH],
+                   [0, MOVING_HEIGHT, MOVING_HEIGHT, 0, 0], color = 'b')
+    line5 = Line2D([-S_WIDTH, -S_WIDTH, S_WIDTH, S_WIDTH, -S_WIDTH], [0, S_HEIGHT, S_HEIGHT, 0, 0], color = 'b')
+
+    line6 = Line2D([-NARROW_WIDTH, -NARROW_WIDTH, NARROW_WIDTH, NARROW_WIDTH, -NARROW_WIDTH],
+                   [0, NARROW_HEIGHT * 3, NARROW_HEIGHT * 3, 0, 0], color = 'b')
+    line7 = Line2D([-NARROW_WIDTH, NARROW_WIDTH], [NARROW_HEIGHT, NARROW_HEIGHT], color = 'b')
+    line8 = Line2D([-NARROW_WIDTH, NARROW_WIDTH], [NARROW_HEIGHT * 2, NARROW_HEIGHT * 2], color = 'b')
 
     def __init__(self):
         self.HOST = '169.254.248.220'
@@ -18,29 +54,15 @@ class Lidar:
         self.BUFF = 57600
         self.MESG = chr(2) + 'sEN LMDscandata 1' + chr(3)
 
+        self.mode = modes['STATIC_OBS']
+
         self.fig = plt.figure(figsize = (6, 6))
         self.ax1 = self.fig.add_subplot(1, 1, 1)
 
         self.data_list = []
+        self.parsed_data = []
 
-    def set_ip(self, ip): self.HOST = ip
-
-    def set_port(self, port): self.PORT = port
-
-    # ROI_tuple: (theta_1, theta_2, radius)
-    def set_ROI(self, ROI_tuple):
-        self.ROI = ROI_tuple
-        self.xmin = self.ROI[2] * math.cos(math.radians(self.ROI[1]))
-        self.xmax = self.ROI[2] * math.cos(math.radians(self.ROI[0]))
-
-        self.x1 = numpy.arange(self.xmin, 0.05, 0.05)
-        self.x2 = numpy.arange(0, self.xmax + 0.05, 0.05)
-        self.x3 = numpy.arange(self.xmin, self.xmax + 0.05, 0.05)
-
-        self.y1 = [(math.tan(math.radians(self.ROI[1])) * v) for v in self.x1]
-        self.y2 = [(math.tan(math.radians(self.ROI[0])) * v) for v in self.x2]
-        self.y3 = [(math.sqrt(self.ROI[2] ** 2 - v ** 2)) for v in self.x3]
-
+    def set_mode(self, mode): self.mode = mode
 
     def loop(self):
         self.sock_lidar = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -53,47 +75,116 @@ class Lidar:
             # 아래 줄은 그 응답 코드을 무시하고 바로 데이터를 받기 위해서 존재함
             if data.__contains__('sEA'): continue
 
-            self.data_list = data.split(' ')[26:567]
+            self.data_list = data.split(' ')[116:477]
+
+            if self.mode == modes['STATIC_OBS']:
+                try:
+                    self.parsed_data = [[], [], []]
+
+                    for i in range(0, 361):
+                        r = int(self.data_list[i], 16) / 10
+
+                        if r >= 1:
+                            x = r * math.cos(math.radians(0.5 * i))
+                            y = r * math.sin(math.radians(0.5 * i))
+
+                            if -self.STATIC_WIDTH <= x <= self.STATIC_WIDTH and y < self.STATIC_HEIGHT * 3:
+                                self.parsed_data[int(y / self.STATIC_HEIGHT)].append((x, y))
+                except: pass
+
+            elif self.mode == modes['MOVING_OBS']:
+                try:
+                    self.parsed_data = [[]]
+
+                    for i in range(0, 361):
+                        r = int(self.data_list[i], 16) / 10
+
+                        if r >= 1:
+                            x = r * math.cos(math.radians(0.5 * i))
+                            y = r * math.sin(math.radians(0.5 * i))
+
+                            if -self.MOVING_WIDTH <= x <= self.MOVING_WIDTH and y < self.MOVING_HEIGHT:
+                                self.parsed_data[0].append((x, y))
+                except: pass
+
+            elif self.mode == modes['S_CURVE']:
+                try:
+                    self.parsed_data = [[]]
+
+                    for i in range(0, 361):
+                        r = int(self.data_list[i], 16) / 10
+
+                        if r >= 1:
+                            x = r * math.cos(math.radians(0.5 * i))
+                            y = r * math.sin(math.radians(0.5 * i))
+
+                            if -self.S_WIDTH <= x <= self.S_WIDTH and y < self.S_HEIGHT:
+                                self.parsed_data[0].append((x, y))
+                except: pass
+
+            elif self.mode == modes['NARROW']:
+                try:
+                    self.parsed_data = [[], [], []]
+
+                    for i in range(0, 361):
+                        r = int(self.data_list[i], 16) / 10
+
+                        if r >= 1:
+                            x = r * math.cos(math.radians(0.5 * i))
+                            y = r * math.sin(math.radians(0.5 * i))
+
+                            if -self.NARROW_WIDTH <= x <= self.NARROW_WIDTH and y < self.NARROW_HEIGHT * 3:
+                                self.parsed_data[int(y / self.NARROW_HEIGHT)].append((x, y))
+                except: pass
 
     def initiate(self):
-        t = threading.Thread(target = self.loop)
-        t.start()
+        receiving_thread = threading.Thread(target = self.loop)
+        receiving_thread.start()
 
-    def get_data(self):
-        parsed_data = []
-
-        for n in range(2 * self.ROI[0] + 90, 2 * self.ROI[1] + 91):
-            if 3 <= int(self.data_list[n], 16) / 10 and int(self.data_list[n], 16) / 10 <= self.ROI[2]:
-                parsed_data.append((int(self.data_list[n], 16) / 10, -45 + 0.5 * n))
-
-        return parsed_data
+    def get_data(self): return self.parsed_data
 
     def animate(self, i):
         try:
             xs = []
             ys = []
 
-            for n in range(2 * self.ROI[0] + 90, 2 * self.ROI[1] + 91):
-                if 3 <= int(self.data_list[n], 16) / 10 and int(self.data_list[n], 16) / 10 <= self.ROI[2]:
-                    xs.append(int(self.data_list[n], 16) * math.cos(math.radians(0.5 * n - 45)) / 10)
-                    ys.append(int(self.data_list[n], 16) * math.sin(math.radians(0.5 * n - 45)) / 10)
+            for n in range(0, 361):
+                r = int(self.data_list[n], 16) / 10
 
-            # 이전에 찍었던 점들을 모두 지움
+                if r >= 1:
+                    x = r * math.cos(math.radians(0.5 * n))
+                    y = r * math.sin(math.radians(0.5 * n))
+
+                    xs.append(x)
+                    ys.append(y)
+
             self.ax1.clear()
-
-            self.ax1.plot(xs, ys, 'ro', markersize = 2)
+            self.ax1.plot(xs, ys, 'ro', markersize = 2, zorder = 10)
 
             # ROI 경계선 그리기: 개발자가 보기 편하도록
-            self.ax1.plot(self.x1, self.y1, 'b', linewidth = 1)
-            self.ax1.plot(self.x2, self.y2, 'b', linewidth = 1)
-            self.ax1.plot(self.x3, self.y3, 'b', linewidth = 1)
+            self.ax1.add_line(self.line0)
+
+            if self.mode == modes['STATIC_OBS']:
+                self.ax1.add_line(self.line1)
+                self.ax1.add_line(self.line2)
+                self.ax1.add_line(self.line3)
+
+            elif self.mode == modes['MOVING_OBS']:
+                self.ax1.add_line(self.line4)
+
+            elif self.mode == modes['S_CURVE']:
+                self.ax1.add_line(self.line5)
+
+            elif self.mode == modes['NARROW']:
+                self.ax1.add_line(self.line6)
+                self.ax1.add_line(self.line7)
+                self.ax1.add_line(self.line8)
 
             # 축 범위 지정하기
-            self.ax1.set_xlim(-100, 100)
-            self.ax1.set_ylim(0, 200)
+            self.ax1.set_xlim(-200, 200)
+            self.ax1.set_ylim(-100, 300)
 
-        except:
-            pass
+        except: pass
 
     def plot_data(self):
         anim = animation.FuncAnimation(self.fig, self.animate, interval = 1)
@@ -102,7 +193,10 @@ class Lidar:
 
 if __name__ == "__main__":
     current_lidar = Lidar()
-    current_lidar.set_ROI((0, 180, 80))
     current_lidar.initiate()
+    current_lidar.set_mode(modes['NARROW'])
 
-    current_lidar.plot_data()
+    while True:
+        print(current_lidar.get_data())
+
+    #current_lidar.plot_data()
