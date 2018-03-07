043533643375f15cbcd7366fc26411da8ea3bc8c
HEVEN2018 <elpion19@gmail.com>
2018-03-07 20:53:02 +0900

    main

--- main.py
+++ main.py
@@ -1,41 +1,24 @@
 # 2018 국제대학생창작자동차대회 자율주행차 부문 성균관대학교 HEVEN 팀
 # interpreted by python 3.6
-
 # 하위 프로그램
-import lidar
-import sign_cam
-import lane_cam
-import path_planner
-import car_control
-import communication
-import lane_cam_noclass
-# module
-import numpy
-import cv2
-import pycuda.autoinit
-import pycuda.driver as cuda
-from multiprocessing import pool, process
-import threading
+import lidarstart
+
+
 
 
 
 from pycuda.compiler import SourceModule
+#module
+import threading
 
 # cpu 병렬 처리
-
-def getFunction(name, contents): # name은 함수의 이름, contents는 함수의 본체여야 합니다.
-    mod = SourceModule(contents) # mod에 contents를 불러옵니다.
-    return mod.get_function(name) # mod에서 이름이 name인 함수를 반환합니다.
+modes = {'DEFAULT': 0, 'PARKING': 1, 'STATIC_OBS': 2,
+         'MOVING_OBS': 3, 'S_CURVE': 4, 'NARROW': 5, 'U_TURN': 6, 'CROSS_WALK': 7}
 
 
 def main():
 
-    lane_detection_thread=threading.Thread(target=lane_cam_noclass)
-    sign_cam_thread=threading.Thread(target=sign_cam.main())
-    read_lidar_thread1=threading.Thread(target=lidar.current_lidar.set_ROI(80,300))#클래스로 되어있어 스레딩 처리 따로 되어있어야할듯
-    read_lidar_thread2=threading.Thread(target=lidar.current_lidar.initiate())
-    read_lidar_thread3=threading.Thread(target=lidar.current_lidar.set_mode(1))
-    read_lidar_thread4=threading.Thread(target=lidar.current_lidar.plot_data())#물어보고 진행
+    lidarstart_thread=threading.Thread(target=lidarstart.lidarstart())
 
 
 
@@ -43,7 +26,17 @@ def main():
 
 
 
-    Lidar_thread = threading.Thread(target=read_Lidar())
+    lidarstart_thread.start()
+
+
+
+
+    # lane_detection_thread=threading.Thread(target=lane_cam_noclass) 미완성
+    #sign_cam_thread = threading.Thread(target=sign_cam.main())
+
+
+
+    '''
     Matrix_thread = threading.Thread(target=matrix_Comb())
     AS_thread = threading.Thread(target=astar_Comb())
     # IMU_thread = threading.Thread(target = read_IMU())
@@ -54,43 +47,20 @@ def main():
     Show_thread = threading.Thread(target=show_Path())
     Obs_thread = threading.Thread(target=front_Detect())
     # U_thread = threading.Thread(target = uturn_detect())
->>>>>>> Stashed changes
-    # 센서 값 받기
-    # 라이다
-    # 차선 비전
-    # 표지판 비전
+    '''
 
-    # 플래닝
-    # 제어
 
-    # 통신
+# 센서 값 받기
+# 라이다
+# 차선 비전
+# 표지판 비전
 
-<<<<<<< Updated upstream
-    pass
-=======
-    pass
+# 플래닝
+# 제어
+
+# 통신
 
 if __name__ == "__main__":
-    start_time = 0
-    end_time = 0
-    while True:
-        start_time = time.time()
-        print(start_time - end_time)
-        end_time= time.time()
-        # openCam()
-        main()
-        # astar_Comb()
-        dim = (500, 500)
-        # map_plot = cv2.resize(lane_Comb, dim, interpolation = cv2.INTER_AREA)
-        # lidar_plot = cv2.resize(lidar_Comb, dim, interpolation = cv2.INTER_AREA)
-        lane_plot = cv2.resize(matrix_Show, dim, interpolation=cv2.INTER_AREA)
-        # cv2.imshow('map', map_plot)
-        # cv2.imshow('lidar', lidar_plot)
-        cv2.imshow('Path', lane_plot)
-        # cv2.imshow('cam', img)
-        if cv2.waitKey(1) == 27:
-            break
-    cam.release()
-    cv2.destroyAllWindows()
-    cv2.waitKey(0)
+    main()
+
 
