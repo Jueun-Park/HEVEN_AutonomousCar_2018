ab92e9205e030578dafb53d6380cf6c9e672c960
HEVEN2018 <elpion19@gmail.com>
2018-03-12 08:56:58 +0900

    옮기는중

--- Multiprocessing/go.py
+++ Multiprocessing/go.py
@@ -1,5 +1,5 @@
 from multiprocessing import Process
-
+import os
 def GO():
     print('Hi\n')
     
@@ -8,7 +8,10 @@ def GO2():
 if __name__=='__main__':
     pr1 = Process(target=GO)
     pr2 = Process(target=GO2)
+
     pr1.start()
     pr2.start()
+
+
     pr1.join()
     pr2.join()
--- signcampackage/signmain.py
+++ signcampackage/signmain.py
@@ -6,12 +6,11 @@ from Multiprocessing import go
 def main() :
     U_Process = Process(target=signuturn.sign_camstart())
     Cros_Process=Process(target=signcros.sign_camstart())
-    test = Process(target=go.GO())
+    #test = Process(target=go.GO())
 
     U_Process.start()
-
     Cros_Process.start()
-    test.start()
+    #test.start()
 
 
 if __name__ == "__main__":
