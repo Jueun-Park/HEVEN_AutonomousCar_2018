2d16e572a6392a9fedeb86f6158be821f8164afc
HEVEN2018 <elpion19@gmail.com>
2018-03-19 11:01:35 +0900

    옮기는중

--- signmain.py
+++ signmain.py
@@ -14,19 +14,19 @@ import threading
 def main() :
 
 
-    #U_Process = threading.Thread(target=signuturn.sign_camstart)
-    Cros_Process=threading.Thread(target=signcros.sign_camstart)
-    Mov_Process=threading.Thread(target=signmov.sign_camstart)
-    #Park_Process=Process(target=signpark.sign_camstart)
-    #static_Process=Process(target=signstatic.sign_camstart)
-    #narrow_Process=Process(target=signnarrow.sign_camstart)
+    U_Process = Process(target=signuturn.sign_camstart)
+    Cros_Process=Process(target=signcros.sign_camstart)
+    Mov_Process=Process(target=signmov.sign_camstart)
+    Park_Process=Process(target=signpark.sign_camstart)
+    static_Process=Process(target=signstatic.sign_camstart)
+    narrow_Process=Process(target=signnarrow.sign_camstart)
 
-    #U_Process.start()
+    U_Process.start()
     Cros_Process.start()
     Mov_Process.start()
-    #Park_Process.start()
-    #static_Process.start()
-    #narrow_Process.start()
+    Park_Process.start()
+    static_Process.start()
+    narrow_Process.start()
 
 
 
