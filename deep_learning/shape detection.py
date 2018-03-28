e795741b5c00947b4a9c549e909a4860bd3e0fab
이아영 <hinealigh22@naver.com>
2018-03-28 20:39:58 +0900

    수정

--- deep_learning/shape detection.py
+++ deep_learning/shape detection.py
@@ -22,33 +22,39 @@ if cap.read():
             print("image is none")
         else:
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            gray = cv2.GaussianBlur(gray, (21, 21), 0)
-            ret, thresh = cv2.threshold(gray, 127, 255, 1)
+            blur = cv2.GaussianBlur(gray, (21, 21), 0)
+            ret, thresh = cv2.threshold(blur, 127, 255, 1)
 
             image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             count = 0
             for cnt in contours:
-                name = './images/new.jpg' + str(count)+ '_'+str(count_2)+ '.jpg'
+                name = './images/new' + str(count)+ '_'+str(count_2)+ '.jpg'
                 (x, y, w, h) = cv2.boundingRect(cnt)
 
                 cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
                 le = max(w,h)
 
-                stad = [64, 128, 192, 256, 320, 384, 448]
+                if w>75 and h>75:
 
-                for i in stad:
-                    if i-64<le<i:
-                        x_1 = int (x+(w-i)/2)
-                        x_2 = int (x+(w+i)/2)
-                        y_1 = int (y+(h-i)/2)
-                        y_2 = int (y+(h+i)/2)
+#                stand = [64, 128, 192, 256, 320, 384, 448]
 
-                        img_trim = img[y_1 : y_2,x_1 :x_2]
+#                for i in stand:
+#                    if i<le<i+64:
+#                        x_1 = int (x+(w-i)/2)q
+#                        x_2 = int (x+(w+i)/2)
+#                        y_1 = int (y+(h-i)/2)
+#                        y_2 = int (y+(h+i)/2)
+                    x_1 = int (x+(w-le)/2)
+                    x_2 = int (x+(w+le)/2)
+                    y_1 = int (y+(h-le)/2)
+                    y_2 = int (y+(h+le)/2)
 
-                        height, width = img_trim.shape[:2]
-                        image = cv2.resize(img_trim, (width*32/i,height*32/i),interpolation=cv2.INTER_AREA)
+                    img_trim = img[y_1 : y_2 , x_1 :x_2]
+#                        image = cv2.resize(img_trim,(32,32),interpolation=cv2.INTER_AREA)
+                    height, width = img_trim.shape[:2]
 
-                cv2.imwrite(name,image)
+                    if height > 75 and width> 75:
+                        cv2.imwrite(name,img_trim)
 
                 count += 1
         cv2.imshow('img', img)
