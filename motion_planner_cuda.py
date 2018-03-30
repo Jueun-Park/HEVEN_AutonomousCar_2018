fe30b2a72fe43a8b23fc87c53c2a7d8a07fe7e1f
HEVEN2018 <elpion19@gmail.com>
2018-03-30 15:47:50 +0900

    motion_planner에 pycuda적용 (43배 속도 개선; 0.13s -> 0.003s)

---
+++ motion_planner_cuda.py
@@ -0,0 +1,107 @@
+# 경로 설정
+# input: 1. numpy array (from lidar)
+#        2. numpy array (from lane_cam)
+# output: 차선 center 위치, 기울기, 곡률이 담긴 numpy array
+import pycuda.autoinit
+import pycuda.driver as drv
+import numpy as np
+from pycuda.compiler import SourceModule
+
+import cv2
+import threading
+from lidar import Lidar
+import time
+
+np.set_printoptions(linewidth=1000)
+
+mod = SourceModule(r"""
+    #include <stdio.h>
+    #include <math.h>
+
+    #define PI 3.14159265
+    __global__ void detect(int data[][2], int *rad, unsigned char frame[][1000])
+    {
+            for(int r = 0; r < rad[0]; r++) {
+                const int thetaIdx = threadIdx.x;
+                const int theta = thetaIdx * 5;
+                int x = rad[0] + int(r * cos(theta * PI/180)) - 1;
+                int y = rad[0] - int(r * sin(theta * PI/180)) - 1;
+
+                if (data[thetaIdx][0] == 0) data[thetaIdx][1] = r;
+                if (frame[y][x] != 0) data[thetaIdx][0] = 1;
+            }
+    } 
+    """)
+path = mod.get_function("detect")
+
+class MotionPlanner():
+
+    def __init__(self, lidar_instance):
+        self.lidar = lidar_instance
+
+    def loop(self):
+        Rad=np.int32(current_lidar.RADIUS)
+        while True:
+            t1 = time.time()
+            data = np.zeros((37, 2), np.int)
+            current_frame = self.lidar.frame
+
+            if current_frame is not None:
+                path(drv.InOut(data), drv.In(Rad), drv.In(current_frame),
+                    block=(37,1,1))
+
+                for i in range(0, 37):
+                    x = Rad + int(round(data[i][1] * np.cos(np.radians(i * 5)))) - 1
+                    y = Rad - int(round(data[i][1] * np.sin(np.radians(i * 5)))) - 1
+                    cv2.line(current_frame, (Rad, Rad), (x, y), 255)
+
+                cv2.imshow('lidar', current_frame)
+                t2 = time.time()
+                print(t2 - t1)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'): break
+
+
+    def initiate(self):
+        thread = threading.Thread(target=self.loop)
+        thread.start()
+
+
+
+current_lidar = Lidar()
+current_lidar.initiate()
+
+motion_plan = MotionPlanner(current_lidar)
+motion_plan.loop()
+
+'''
+canvas = np.zeros((400, 400), np.uint8)
+data = np.zeros((2, 19), np.int)
+
+cv2.circle(canvas, (300, 150), 70, 255, -1)
+
+for r in range(0, 600):
+    for theta in range(0, 91, 5):
+        x = int(r * np.cos(np.radians(theta)))
+        y = int(r * np.sin(np.radians(theta)))
+
+        if x >= 400 or y >= 400: continue
+
+        if data[0][int(theta / 5)] == 0:
+            data[1][int(theta / 5)] = r
+
+        if canvas[y][x] != 0:
+            data[0][int(theta / 5)] = 1
+
+print(data)
+
+for i in range(0, 19):
+    x = int(data[1][i] * np.cos(np.radians(i * 5)))
+    y = int(data[1][i] * np.sin(np.radians(i * 5)))
+    cv2.line(canvas, (0, 0), (x, y), 255)
+
+while True:
+    cv2.imshow('canvas', canvas)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'): break
+'''
