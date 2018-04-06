266d8235239ca7ddecf5a10a6b1fa926e074bada
Junheyok, Park <vzx00990@gmail.com>
2018-04-06 22:00:01 +0900

    refined - 실험용

--- car_control_test.py
+++ car_control_test.py
@@ -58,14 +58,18 @@ class Control:
         self.default_y_dis = 1  # (임의의 값 / 1m)
 
         if self.mission_num == 0:
-            self.cross_track_error = first/100
-            self.linear = second
+            if self.mode == 0:
+                self.cross_track_error = first/100
+                self.linear = second
+                
+                self.__default1__()
 
-            # cul을 이용할때 정의함 - 실험 할 때 필요한 데이터 값 입력
-            #self.linear = second[0]
-            #self.cul = second[1]
+            else:
+                self.cross_track_error = first/100
+                self.linear = second[0]
+                self.cul = second[1]
 
-            self.__default__()
+                self.__default2__()
 
         elif self.mission_num == 2:
             self.obs_r = first[0]
@@ -105,82 +109,83 @@ class Control:
 
             self.__cross__()
 
-    def __default__(self):
+    def __default1__(self):
         self.steer = 0
         self.speed = 54
         self.gear = 0
         self.brake = 0
 
-        if self.mode == 0:
-            self.tan_value = self.linear * (-1)
-            self.theta_1 = math.degrees(math.atan(self.tan_value))
+        self.tan_value = self.linear * (-1)
+        self.theta_1 = math.degrees(math.atan(self.tan_value))
 
-            k = 1
-            if abs(self.theta_1) < 15 and abs(self.cross_track_error) < 0.27:
-                k = 0.5
+        k = 1
+        if abs(self.theta_1) < 15 and abs(self.cross_track_error) < 0.27:
+            k = 0.5
 
-            self.velocity = (self.speed_platform * 100) / 3600
+        self.velocity = (self.speed_platform * 100) / 3600
 
-            self.theta_2 = math.degrees(math.atan((k * self.cross_track_error) / self.velocity))
+        self.theta_2 = math.degrees(math.atan((k * self.cross_track_error) / self.velocity))
 
-            self.adjust = 0.1
+        self.adjust = 0.1
 
-            steer_now = (self.theta_1 + self.theta_2)
-            steer_final = (self.adjust * self.steer_past) + ((1 - self.adjust) * steer_now)
+        steer_now = (self.theta_1 + self.theta_2)
+        steer_final = (self.adjust * self.steer_past) + ((1 - self.adjust) * steer_now)
 
-            self.steer = steer_final * 71
+        self.steer = steer_final * 71
 
-            self.steer_past = steer_final
+        self.steer_past = steer_final
 
-            if self.steer > 1970:
-                self.steer = 1970
-                self.steer_past = 27.746
-            elif self.steer < -1970:
-                self.steer = -1970
-                self.steer_past = -27.746
+        if self.steer > 1970:
+            self.steer = 1970
+            self.steer_past = 27.746
+        elif self.steer < -1970:
+            self.steer = -1970
+            self.steer_past = -27.746
 
-        else:
-            self.steer = 0
-            self.speed = 54
-            self.gear = 0
-            self.brake = 0
+        return self.steer, self.speed, self.gear, self.brake, self.steer_past
 
-            self.tan_value_1 = abs(self.linear)
-            self.theta_1 = math.atan(self.tan_value_1)
+    def __default2__(self):
+        self.steer = 0
+        self.speed = 54
+        self.gear = 0
+        self.brake = 0
 
-            self.son = self.cul * math.sin(self.theta_1) - self.default_y_dis
-            self.mother = self.cul * math.cos(self.theta_1) + self.cross_track_error + 0.4925
+        self.tan_value_1 = abs(self.linear)
+        self.theta_1 = math.atan(self.tan_value_1)
 
-            self.tan_value_2 = abs(self.son / self.mother)
-            self.theta_line = math.degrees(math.atan(self.tan_value_2))
+        self.son = self.cul * math.sin(self.theta_1) - self.default_y_dis
+        self.mother = self.cul * math.cos(self.theta_1) + self.cross_track_error + 0.4925
 
-            if self.linear > 0:
-                self.theta_line = self.tan_value_2 * (-1)
+        self.tan_value_2 = abs(self.son / self.mother)
+        self.theta_line = math.degrees(math.atan(self.tan_value_2))
 
-            k = 1
+        if self.linear > 0:
+            self.theta_line = self.tan_value_2 * (-1)
 
-            if abs(self.theta_line) < 15 and abs(self.cross_track_error) < 0.27:
-                k = 0.5
+        k = 1
 
-            self.velocity = (self.speed_platform * 100) / 3600
+        if abs(self.theta_line) < 15 and abs(self.cross_track_error) < 0.27:
+            k = 0.5
 
-            self.theta_error = math.degrees(math.atan((k * self.cross_track_error) / self.velocity))
+        self.velocity = (self.speed_platform * 100) / 3600
 
-            self.adjust = 0.1
+        self.theta_error = math.degrees(math.atan((k * self.cross_track_error) / self.velocity))
 
-            steer_now = (self.theta_line + self.theta_error)
-            steer_final = (self.adjust * self.steer_past) + ((1 - self.adjust) * steer_now) * 1.387
+        self.adjust = 0.1
 
-            self.steer = steer_final * 71
+        steer_now = (self.theta_line + self.theta_error)
+        steer_final = (self.adjust * self.steer_past) + ((1 - self.adjust) * steer_now) * 1.387
 
-            self.steer_past = steer_final
+        self.steer = steer_final * 71
 
-            if self.steer > 1970:
-                self.steer = 1970
-                self.steer_past = 27.746
-            elif self.steer < -1970:
-                self.steer = -1970
-                self.steer_past = -27.746
+        self.steer_past = steer_final
+
+        if self.steer > 1970:
+            self.steer = 1970
+            self.steer_past = 27.746
+        elif self.steer < -1970:
+            self.steer = -1970
+            self.steer_past = -27.746
 
         return self.steer, self.speed, self.gear, self.brake, self.steer_past
 
