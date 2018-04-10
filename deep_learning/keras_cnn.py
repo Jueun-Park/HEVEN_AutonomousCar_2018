999bc6dbfd03a005b8d4927f0c3b45db1bf454ae
Jiwung Hyun <kabbi159@gmail.com>
2018-04-10 21:35:29 +0900

    cnn_main 코드 추가 및 keras 코드 추가 (keras로 한번 실험해볼예정)

--- deep_learning/keras_cnn.py
+++ deep_learning/keras_cnn.py
@@ -45,15 +45,21 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
+from keras import optimizers
+from keras.models import Model
+from keras.layers import Input, Flatten, Dropout, Concatenate, Activation
+from keras.layers import Convolution2D, MaxPooling2D, AveragePooling2D
+from keras.layers import GlobalMaxPooling2D, GlobalAveragePooling2D
+from keras.applications.imagenet_utils import _obtain_input_shape
 
 
 # dimensions of our images.
-img_width, img_height = 150, 150
+img_width, img_height = 32, 32
 
-train_data_dir = 'data/train'
-validation_data_dir = 'data/validation'
-nb_train_samples = 2000
-nb_validation_samples = 800
+train_data_dir = 'data/TRAIN_DIR'
+validation_data_dir = 'data/VALIDATION_DIR'
+nb_train_samples = 7000
+nb_validation_samples = 700
 epochs = 50
 batch_size = 16
 
@@ -62,6 +68,7 @@ if K.image_data_format() == 'channels_first':
 else:
     input_shape = (img_width, img_height, 3)
 
+'''
 model = Sequential()
 model.add(Conv2D(32, (3, 3), input_shape=input_shape))
 model.add(Activation('relu'))
@@ -81,9 +88,64 @@ model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
+'''
 
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
+model = Sequential()
+model.add(Conv2D(32, (1, 1), padding='valid',
+                 input_shape=input_shape))
+model.add(Activation('relu'))
+#model.add(Conv2D(32, (2, 2), padding='same'))
+#model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (1, 1), padding='valid'))
+model.add(Activation('relu'))
+#model.add(Conv2D(64, (2, 2),padding='same'))
+#model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7))
+model.add(Activation('softmax'))
+
+
+'''
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same', input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('softmax'))
+'''
+
+sgd = optimizers.Adagrad(lr=0.01, epsilon=None, decay=0.0)
+
+
+
+
+
+model.compile(loss='mean_squared_error',
+              optimizer= sgd,
               metrics=['accuracy'])
 
 # this is the augmentation configuration we will use for training
@@ -116,4 +178,4 @@ model.fit_generator(
     validation_data=validation_generator,
     validation_steps=nb_validation_samples // batch_size)
 
-model.save_weights('first_try.h5')
+model.save_weights('seven_class_squeeze.h5')
