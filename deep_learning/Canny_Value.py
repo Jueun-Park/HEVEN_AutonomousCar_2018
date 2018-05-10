38a76cd5f04c49507338d630a975c5e9462b3fac
이아영 <hinealigh22@naver.com>
2018-05-10 15:48:40 +0900

    실험용.update

--- deep_learning/Canny_Value.py
+++ deep_learning/Canny_Value.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+cap.set(3, 800)
+cap.set(4, 448)
 
 def nothing(x):
     pass
@@ -28,6 +30,32 @@ if cap.read():
             img5 = cv2.GaussianBlur(img, (5, 5), 0)
             gray = cv2.cvtColor(img5, cv2.COLOR_BGR2GRAY)
             edges = cv2.Canny(gray, threshold1, threshold2)
+            image, contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            edges_copy = edges
+            count = 0
+
+            for cnt in contours:
+                (x, y, w, h) = cv2.boundingRect(cnt)
+                le = max(w,h)
+
+                if w>60 and h>60:
+
+                    x_1 = int (x+(w-le)/2 -5)
+                    x_2 = int (x+(w+le)/2 +5)
+                    y_1 = int (y+(h-le)/2 -5)
+                    y_2 = int (y+(h+le)/2 +5)
+
+                    cv2.rectangle(edges_copy, (x_1, y_1), (x_2, y_2), (255, 0, 0), 5)
+                    img_trim = img[y_1 : y_2 , x_1 :x_2]
+                    height, width = img_trim.shape[:2]
+                    limit = height - width
+
+                    if limit > -5 and limit < 5:
+                        if x_1 >0 and y_1 > 0:
+                            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                            img_trim = cv2.resize(img_trim, (32,32))
 
         cv2.imshow('image', edges)
         if cv2.waitKey(25) & 0xFF == ord('q'):
