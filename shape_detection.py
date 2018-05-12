5e52b97f15eb7a713c0ddebdf6a9df2f492a64e7
이아영 <hinealigh22@naver.com>
2018-05-13 02:04:42 +0900

    정확도 높임. soilidity ratio 이용. y 좌표 제한.

--- shape_detection.py
+++ shape_detection.py
@@ -4,8 +4,6 @@
 # output: 표지판 후보 이미지
 
 import cv2
-global count
-import time
 
 def shape_detect(img):
     sign = []
@@ -15,24 +13,27 @@ def shape_detect(img):
     else:
         img5 = cv2.GaussianBlur(img, (5, 5), 0)
         gray = cv2.cvtColor(img5, cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(gray, 129, 194)
+        edges = cv2.Canny(gray, 52, 104, apertureSize=3)
         image, contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
             (x, y, w, h) = cv2.boundingRect(cnt)
             le = max(w, h) + 10
 
-            if w > 40 and h > 40:
-                x_1 = int(x + (w - le) / 2 - 5)
-                x_2 = int(x + (w + le) / 2 + 5)
-                y_1 = int(y + (h - le) / 2 - 5)
-                y_2 = int(y + (h + le) / 2 + 5)
-                img_trim = img[y_1: y_2, x_1:x_2]
-                height, width = img_trim.shape[:2]
-                limit = height - width
+            area = cv2.contourArea(cnt)
+            hull = cv2.convexHull(cnt)
+            hull_area = cv2.contourArea(hull)
 
-                if -5 < (height - width) < 5:
-                    if x_1 > 0 and y_1 > 0:
-                        cv2.rectangle(img, (x_1, y_1), (x_2, y_2), (255, 0, 0), 4)
+            if hull_area >0:
+                solidity = int (100*area / hull_area)
+                if  solidity>95 and w > 40 and h > 40:
+                    x_1 = int(x + (w - le) / 2 - 5)
+                    x_2 = int(x + (w + le) / 2 + 5)
+                    y_1 = int(y + (h - le) / 2 - 5)
+                    y_2 = int(y + (h + le) / 2 + 5)
+
+                    if x_1 > 0 and 350 > y_2 and y_1 > 110:
+                        img_trim = img[y_1: y_2, x_1:x_2]
+                        # cv2.rectangle(img, (x_1, y_1), (x_2, y_2), (0, 0, 255), 4)
                         img_trim_resize = cv2.resize(img_trim, (32, 32))
                         sign.append(img_trim_resize)
     return sign
@@ -45,17 +46,15 @@ def main():
 
 if __name__ == "__main__":
     # open cam
-    cam = cv2.VideoCapture(0)
+    cam = cv2.VideoCapture(2)
     cam.set(3, 800)
     cam.set(4, 448)
 
-    # count = 0
     if not cam.isOpened():
         print("cam open failed")
     while True:
         s, img = cam.read()
         main()
-        # count += 1
         cv2.imshow('cam', img)
 
         if cv2.waitKey(30) & 0xff == 27:
