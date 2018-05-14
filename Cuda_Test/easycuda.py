2e5b8f89508f28f9c4109e36da104427213c0f0a
Jueun <elpion19@gmail.com>
2018-05-14 21:27:27 +0900

    delete useless directory

--- Cuda_Test/easycuda.py
+++
@@ -1,9 +0,0 @@
-import pycuda.autoinit
-import pycuda.driver as cuda
-import numpy
-
-from pycuda.compiler import SourceModule
-
-def getFunction(name, contents):
-    mod = SourceModule(contents)
-    return mod.get_function(name)
